bf0fbafad854ecb628ee2df651b45b04d8fbb422
mohamed atef <mo7amed3atf24@gmail.com>
2025-08-22 18:36:05 +0300

    Revert "Merge pull request #12 from ahmed-rabi/settings-edit-preview"
    This reverts commit 8441606d448a6542bf6a2df0f09062b030673dc7, reversing
    changes made to a449b4010cca236ef8ec6b85b1aaedc3164ae0d6.

--- src/main/java/com/example/demo/repository/StaticPageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.StaticPage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StaticPageRepository extends JpaRepository<StaticPage, Integer> {
-}
